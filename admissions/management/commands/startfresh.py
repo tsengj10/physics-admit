d092c24b47dc2e5bbf648c96a0039c1e4262f755
Jeff Tseng <jeff.tseng@physics.ox.ac.uk>
2015-10-06 07:48:37 +0100

    boostrap, mostly

--- admissions/management/commands/startfresh.py
+++ admissions/management/commands/startfresh.py
@@ -14,6 +14,7 @@ class Command(BaseCommand):
     load_school_types()
     load_qual_types()
     create_college_groups()
+    # may want to load a user table as well
     ovs = OverallState(state=OverallState.DEVELOPMENT)
     ovs.save()
 
@@ -25,14 +26,15 @@ def load_college_table():
       if firstline:
         firstline = 0
       else:
-        p = College(name = row[0], \
-                    adss_code = row[1], \
-                    total_places = int(row[2]), \
-                    physphil_places = int(row[3]), \
-                    key = row[4]
-                    )
-        p.save()
-        print('Created college {}'.format(p))
+        p, created = College.objects.get_or_create(adss_code=row[1],
+                       defaults={'name' : row[0],
+                                 'total_places' : int(row[2]),
+                                 'physphil_places' : int(row[3]),
+                                 'key' : row[4]})
+        if created:
+          print('Created college {}'.format(p))
+        else:
+          print('Updated college {}'.format(p))
 
 def load_school_types():
   firstline = 1
@@ -42,11 +44,13 @@ def load_school_types():
       if firstline:
         firstline = 0
       else:
-        p = PhysicsSchoolType(adss_type = row[0], \
-                              adss_description = row[1], \
-                              physics_type = row[2])
-        p.save()
-        print('Created school type {}'.format(p))
+        p, created = PhysicsSchoolType.get_or_create(adss_type=row[0],
+                       defaults={'adss_description' : row[1],
+                                 'physics_type' : row[2]})
+        if created:
+          print('Created school type {}'.format(p))
+        else:
+          print('Updated school type {}'.format(p))
 
 def toBoolean(str):
   if len(str) > 0:
@@ -62,13 +66,15 @@ def load_qual_types():
       if firstline:
         firstline = 0
       else:
-        p = QualificationType(name = row[0],
-                              exam_type = row[1],
-                              dual_award = toBoolean(row[2]),
-                              short_course = toBoolean(row[3]),
-                              short_name = row[4])
-        p.save()
-        print('Created qualification type {}'.format(p))
+        p, created = QualificationType.get_or_create(name=row[0],
+                       defaults={'exam_type' : row[1],
+                                 'dual_award' : toBoolean(row[2]),
+                                 'short_course' : toBoolean(row[3]),
+                                 'short_name' : row[4]})
+        if created:
+          print('Created qualification type {}'.format(p))
+        else:
+          print('Updated qualification type {}'.format(p))
 
 def create_college_groups():
   pns = ['add_logentry',
@@ -82,12 +88,17 @@ def create_college_groups():
     ps.append(Permission.objects.get(codename=pn))
   colleges = College.objects.all()
   for college in colleges:
-    group, created = Group.objects.get_or_create(name=college.adss_code)
+    group, created = Group.objects.get_or_create(name=college.adss_code,
+                       defaults={'permissions' : ps})
     if created:
-      group.permissions = ps
-      group.save()
       print('Created user group "{1}" for "{0}"'.format(college, group))
+    else:
+      print('Updated user group "{1}" for "{0}"'.format(college, group))
   # and then add a read-only group
   group, created = Group.objects.get_or_create(name='Readonly')
-  print('Created Readonly user group')
+  if created:
+    print('Created Readonly user group')
+  else:
+    print('Updated Readonly user group')
+
 
