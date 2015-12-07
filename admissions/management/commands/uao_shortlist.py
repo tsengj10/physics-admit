f43d79c8600db58ea6292a7ddca7f2cd72d7094d
Student Admissions <n.clifford1@physics.ox.ac.uk>
2015-12-07 15:01:00 +0000

    add college2 to UAO report; extend view_schedule to 50 by default

--- admissions/management/commands/uao_shortlist.py
+++ admissions/management/commands/uao_shortlist.py
@@ -11,7 +11,7 @@ class Command(BaseCommand):
     
   def handle(self, *args, **options):
     out = csv.writer(sys.stdout, quoting=csv.QUOTE_ALL)
-    fields = [ 'ucas_id','surname','forenames','pat_total','college1' ]
+    fields = [ 'ucas_id','surname','forenames','pat_total','college1','college2' ]
     out.writerow(fields)
 
     for s in Candidate.objects.all().select_related('info'):
@@ -19,11 +19,14 @@ class Command(BaseCommand):
       pt = s.pat_physics + s.pat_maths
       if s.state == s.STATE_SUMMONED:
         decision = s.college1.adss_code
+        college2 = s.college2.adss_code
       elif s.state == s.STATE_WITHDRAWN:
         decision = 'Withdrawn'
+        college2 = ''
       else:
         decision = 'Desummoned'
+        college2 = ''
 
-      data = [ s.ucas_id, c.surname, c.forenames, pt, decision ]
+      data = [ s.ucas_id, c.surname, c.forenames, pt, decision, college2 ]
       out.writerow(data)
 
